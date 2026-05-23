6d42712302e93ca8f701783431b50924141994d6
Сынша <saburovv055@gmail.com>
2026-05-23 21:17:44 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// Начальная коллекция изображений
+const initialImages = [
+    "https://picsum.photos/id/1015/800/600",
+    "https://picsum.photos/id/1033/800/600",
+    "https://picsum.photos/id/1040/800/600",
+    "https://picsum.photos/id/106/800/600",
+    "https://picsum.photos/id/1074/800/600",
+    "https://picsum.photos/id/133/800/600",
+    "https://picsum.photos/id/201/800/600",
+    "https://picsum.photos/id/251/800/600"
+];
